491083ff827cb348d11ade2af7f9c83383e8ef2d
Samitha Liyanage <samitha.liyanage@gmail.com>
2019-08-09 13:19:05 -0400

    Update htrc-demo.sh

--- Python/topicexplorer-demo/htrc-demo.sh
+++ Python/topicexplorer-demo/htrc-demo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DATA_HOME=$HOME/HTRC/demo/htrc-data
+DATA_HOME=$HOME/HTRC/htrc-id
 SECURE_VOLUME=/media/secure_volume
 
 if [ ! -d "$SECURE_VOLUME" ]; then
@@ -13,14 +13,11 @@ if [ ! -d "$SECURE_VOLUME" ]; then
 fi
 
 echo "Downloading texts from HTRC Data API..."
-htrc download -o $SECURE_VOLUME/volumes htrc-id
+htrc download -o $SECURE_VOLUME/volumes $DATA_HOME
 
 echo "Running topic modeling algorithms..."
 topicexplorer init --htrc --name "HTRC Demo Corpus" $SECURE_VOLUME/volumes/ $SECURE_VOLUME/volumes.ini
 topicexplorer train $SECURE_VOLUME/volumes.ini --iter 50 -k 5 10 20 40 80 --context-type book
-
-echo "Getting metadata..."
-htrc metadata $SECURE_VOLUME/volumes/ > $SECURE_VOLUME/volumes/metadata.json
 
 echo "Launching topic explorer..."
 topicexplorer launch $SECURE_VOLUME/volumes.ini
